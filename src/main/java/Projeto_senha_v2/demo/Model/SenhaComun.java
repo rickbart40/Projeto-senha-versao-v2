5986f00e2baf4b9494f3c8a3360bf55bf2bbd9a2
Rick Sander <ricksander40@gmail.com>
2026-09-03 21:25:03 -0300

    Atualização no tipo senha

--- src/main/java/Projeto_senha_v2/demo/Model/SenhaComun.java
+++ src/main/java/Projeto_senha_v2/demo/Model/SenhaComun.java
@@ -3,6 +3,7 @@ package Projeto_senha_v2.demo.Model;
 import Projeto_senha_v2.demo.Interface.ISenha;
 
 public class SenhaComun implements ISenha {
+    
     @Override
     public String gerarTicket() {
         return String.format(
